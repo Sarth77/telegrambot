5f667a6d5331625cc2c1d34e6bed91a190116011
Sarth77 <sahil3112109695@gmail.com>
2024-02-01 00:29:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -21,6 +21,71 @@ logger = logging.getLogger(__name__)
 
 telethon_client = TelegramClient('session', API_ID, API_HASH)
 
+authenticated_users = set()
+
+def help(update: Update, context: CallbackContext) -> None:
+    """Sends a help message when the command /help is issued
+
+    Arguments:
+        update: update from Telegram
+        context: CallbackContext object that stores commonly used objects in handler callbacks
+    """
+
+    help_message = "This bot is used to automatically enter trades onto your MetaTrader account directly from Telegram. To begin, ensure that you are selected the correct channel or group from your account to use for copying trading.\n\nThis bot supports all trade order types (Market Execution, Limit, and Stop)\n\nAfter an extended period away from the bot, please be sure to re-enter the start command to restart the connection to your MetaTrader account."
+    commands = "List of commands:\n/start : to login in your account\n/help : displays list of commands and example trades\n/trade : takes in user inputted trade for parsing and placement\n/calculate : calculates trade information for a user inputted trade"
+    trade_example = "Example Trades 💴:\n\n"
+    market_execution_example = "Market Execution:\nBUY GBPUSD\nEntry NOW\nSL 1.14336\nTP 1.28930\nTP 1.29845\n\n"
+    limit_example = "Limit Execution:\nBUY LIMIT GBPUSD\nEntry 1.14480\nSL 1.14336\nTP 1.28930\n\n"
+    note = "You are able to enter up to two take profits. If two are entered, both trades will use half of the position size, and one will use TP1 while the other uses TP2.\n\nNote: Use 'NOW' as the entry to enter a market execution trade."
+
+    # sends messages to user
+    update.effective_message.reply_text(help_message)
+    update.effective_message.reply_text(commands)
+    update.effective_message.reply_text(trade_example + market_execution_example + limit_example + note)
+
+    return
+def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation.   
+    
+    Arguments:
+        update: update from Telegram
+        context: CallbackContext object that stores commonly used objects in handler callbacks
+    """
+
+    update.effective_message.reply_text("Command has been canceled.")
+
+    # removes trade from user context data
+    context.user_data['trade'] = None
+
+    return ConversationHandler.END
+
+def error(update: Update, context: CallbackContext) -> None:
+    """Logs Errors caused by updates.
+
+    Arguments:
+        update: update from Telegram
+        context: CallbackContext object that stores commonly used objects in handler callbacks
+    """
+
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+    return
+
+def unknown_command(update: Update, context: CallbackContext) -> None:
+    """Checks if the user is authorized to use this bot or shares to use /help command for instructions.
+
+    Arguments:
+        update: update from Telegram
+        context: CallbackContext object that stores commonly used objects in handler callbacks
+    """
+    if update.effective_message.chat.username not in authenticated_users:
+        update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
+        return
+
+    update.effective_message.reply_text("Unknown command. Use /trade to place a trade or /calculate to find information for a trade. You can also use the /help command to view instructions for this bot.")
+
+    return
+
 async def send_otp(phone_number):
     await telethon_client.connect()
     if not await telethon_client.is_user_authorized():
@@ -29,57 +94,88 @@ async def send_otp(phone_number):
     else:
         return False  # User is already authorized
 
-async def verify_otp(otp_code):
+async def verify_otp(otp_code, phone_number):
     try:
-        await telethon_client.sign_in(code=otp_code)
+        await telethon_client.sign_in(phone=phone_number, code=otp_code)
         return True
     except Exception as e:
-        logger.error(e)
+        print(e)
         return False
-
-def start(update, context):
-    update.message.reply_text("Please send your phone number in international format (e.g., +1234567890)")
-    return PHONE
-
-def phone(update, context):
-    phone_number = update.message.text
+    
+def phone(update, context) -> int:
+    phone_number = update.effective_message.text
+    context.user_data['phone_number'] = phone_number
     is_otp_sent = asyncio.run(send_otp(phone_number))
     if is_otp_sent:
-        update.message.reply_text("An OTP has been sent to your phone. Please enter the OTP.")
+        update.effective_message.reply_text("An OTP has been sent to your phone. Please enter the OTP.")
     else:
-        update.message.reply_text("You are already logged in.")
+        update.effective_message.reply_text("You are already logged in.")
     return OTP
 
 def otp(update, context):
-    otp_code = update.message.text
-    if asyncio.run(verify_otp(otp_code)):
-        update.message.reply_text("You have been successfully connected!")
-        return END
+    otp_code = update.effective_message.text
+    phone_number = context.user_data.get('phone_number')
+    if asyncio.run(verify_otp(otp_code, phone_number)):
+        update.effective_message.reply_text("You have been successfully connected!")
+        return ConversationHandler.END
     else:
-        update.message.reply_text("Invalid OTP. Please try again.")
+        update.effective_message.reply_text("Invalid OTP. Please try again.")
         return OTP
 
-def cancel(update, context):
-    update.message.reply_text("Operation cancelled.")
-    return ConversationHandler.END
+def welcome(update: Update, context: CallbackContext) -> str:
+    """Sends welcome message to user.
 
-def main():
+    Arguments:
+        update: update from Telegram
+        context: CallbackContext object that stores commonly used objects in handler callbacks
+    """
+
+    welcome_message = "Welcome to the FX Signal Copier Telegram Bot! 💻💸\n\nYou can use this bot to enter trades directly from Telegram and get a detailed look at your risk to reward ratio with profit, loss, and calculated lot size.\n\nIn order to get started please send your phone number in international format (e.g., +1234567890)\n\nUse the /help command to view instructions and example trades."
+    
+    # sends messages to user
+    update.effective_message.reply_text(welcome_message)
+    
+    return PHONE
+
+
+
+def main() -> None:
+    """Runs the Telegram bot."""
+
     updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+
+    # get the dispatcher to register handlers
     dp = updater.dispatcher
 
+    # help command handler
+    dp.add_handler(CommandHandler("help", help))
+    
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler('start', welcome)],
         states={
             PHONE: [MessageHandler(Filters.text & ~Filters.command, phone)],
-            OTP: [MessageHandler(Filters.text & ~Filters.command, otp)]
+            OTP: [MessageHandler(Filters.text & ~Filters.command, otp)],
+            # TRADE: [MessageHandler(Filters.text & ~Filters.command, PlaceTrade)],
+            # CALCULATE: [MessageHandler(Filters.text & ~Filters.command, CalculateTrade)],
+            # DECISION: [CommandHandler("yes", PlaceTrade), CommandHandler("no", cancel)]
         },
-        fallbacks=[CommandHandler('cancel', cancel)]
+        fallbacks=[CommandHandler('cancel', cancel)],
     )
 
+    # conversation handler for entering trade or calculating trade information
     dp.add_handler(conv_handler)
 
+    # message handler for all messages that are not included in conversation handler
+    dp.add_handler(MessageHandler(Filters.text, unknown_command))
+
+    # log all errors
+    dp.add_error_handler(error)
+    
+    # listens for incoming updates from Telegram
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TELEGRAM_BOT_TOKEN, webhook_url=APP_URL + TELEGRAM_BOT_TOKEN)
     updater.idle()
+
+    return
 
 if __name__ == '__main__':
     main()
