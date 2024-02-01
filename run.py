0f4ae9b28b013d8737f481292b223f939ba6e656
Sarth77 <sahil3112109695@gmail.com>
2024-02-01 18:15:21 +0530

    Update run.py

--- run.py
+++ run.py
@@ -98,17 +98,15 @@ async def send_otp(update, context):
         update.effective_message.reply_text("You are already logged in.")
         return ConversationHandler.END      
     else:
-        await telethon_client.sign_in(phone = phone_number)
-        context.user_data['login_token'] = login_token
+        await telethon_client.sign_in(phone=phone_number)
         update.effective_message.reply_text("Please enter the OTP.")
         return OTP  # Indicates that OTP was sent
         
 async def otp(update, context):
     otp_code = update.effective_message.text
     phone_number = context.user_data.get('phone_number')
-    login_token = context.user_data.get('login_token')
     try:
-        user_or_token = await telethon_client.sign_in(login_token, code=otp_code)
+        user_or_token = await telethon_client.sign_in(phone=phone_number, code=otp_code)
         update.effective_message.reply_text("You have been successfully connected!",user_or_token)
         return ConversationHandler.END
     except Exception as e:
